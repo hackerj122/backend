30fa444d630061ee42dbb8071f48213a6b8f2a3c
hackerj122 <jaypaithankar12@gmail.com>
2024-03-14 22:37:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+// MySQL connection configuration
+const connection = mysql.createConnection({
+  host: 'bfieq1twbcmxrf2uduv1-mysql.services.clever-cloud.com',
+  user: 'u7lxc4bve9dsddcv',
+  password: 'przsKHxT6d9C3d0sqgzl',
+  database: 'bfieq1twbcmxrf2uduv1',
+});
+
+// Connect to MySQL database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+// Endpoint for user login
+app.post('/login', (req, res) => {
+  const { username, password} = req.body;
+
+  // Query the database to check if the username and password match
+  const query = `SELECT * FROM users WHERE email = ? AND password = ?`;
+  connection.query(query, [username, password], (error, results) => {
+    if (error) {
+      console.error('Error executing MySQL query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+
+    if (results.length === 1) {
+            // If user is found, return user data including name
+            const user = results[0];
+            res.status(200).json({
+              id: user.uniquecode,
+              username: user.email,
+              name: user.firstname + user.lastname,
+              message: 'Login successful'// Include user's name in the response
+              // Add other user data as needed
+            });
+          } else {
+            // If user is not found or password is incorrect, return error
+            res.status(401).json({ error: 'Invalid username or password' });
+          }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
